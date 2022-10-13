34c3156e97c4a43d326040319bd8c8b9059430d4
jguzman-bds <115578368+jguzman-bds@users.noreply.github.com>
2022-10-13 14:03:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 import streamlit as st
 import pandas as pd
 
-conn = create_engine("mssql+pyodbc://dbadmin:Barcelona2020@bdigital.database.windows.net/[dbo]?charset=utf8mb4")
+driver = 'SQL Server Native Client 11.0'
+
+conn = create_engine("mssql+pyodbc://dbadmin:Barcelona2020@bdigital.database.windows.net/DemoBudget?driver=ODBC+Driver+17+for+SQL+Server")
+#conn = create_engine('mssql+pyodbc://(localdb)\ProjectsV12/MainTest1?driver=SQL+Server+Native+Client+11.0', echo=True)
 SQL_Query = pd.read_sql('SELECT [Customer Group],[Importe] FROM [dbo].[FGastosCliente]', conn)
 
+
 st.write("My First App")
-df = pd.DataFrame(SQL_Query, columns=['[Customer Group]','[Importe]'])
+df = pd.DataFrame(SQL_Query, columns=['Customer Group','Importe'])
 st.line_chart(df)
